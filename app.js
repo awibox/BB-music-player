1fdc0bf222d0a34631479a8a10f38edc75f233ba
Andrey Arkhipov <aarkhipov@rutube.ru>
2016-09-02 18:59:26 +0300

    Обработчик треков

---
+++ app.js
@@ -0,0 +1,71 @@
+var data = {
+    sound: [
+        {
+            artist: "Chaz Robinson",
+            name: "Love Will Come Back Again",
+            time: "03:56",
+            src: "mp3/Chaz_Robinson_-_Love_Will_Come_Back_Again.mp3"
+        },
+        {
+            artist: "Funky Stereo",
+            name: "Funny Life",
+            time: "02:55",
+            src: "mp3/Funky_Stereo_-_Funny_Life.mp3"
+        },
+        {
+            artist: "JekK",
+            name: "First",
+            time: "03:24",
+            src: "mp3/JekK_-_First.mp3"
+        },
+        {
+            artist: "Pokki_DJ",
+            name: "Energy",
+            time: "03:50",
+            src: "mp3/Pokki_DJ_-_Energy.mp3"
+        }
+    ]
+};
+
+console.log(data);
+
+for (var i = 0; i < data.sound.length;i++) {
+    var template = '<div class="list__item" data-src="' + data.sound[i].src + '">' + data.sound[i].artist + ' - ' + data.sound[i].name + ' (' + data.sound[i].time + ')</div>';
+    $('.list').append(template);
+}
+
+$('.list__item').click(function(){
+    var PageSound = document.getElementById('PageSound');
+    PageSound.src = $(this).attr('data-src');
+    PageSoundPlay();
+});
+
+function PageSoundPlay()
+{
+    var nowTime = new Date().getTime();
+    var PageSound = document.getElementById('PageSound');
+    localStorage['PageSound'] = nowTime;
+    PageSound.play();
+}
+function PageSoundStop()
+{
+    PageSound.pause();
+    PageSound.currentTime = 0;
+}
+function onStorageEvent(storageEvent){
+    if(storageEvent.key == 'PageSound')
+        PageSoundStop();
+}
+$(document).ready(function(){
+    if (window.addEventListener) {
+        window.addEventListener("storage", onStorageEvent, false);
+    } else {
+        window.attachEvent("onstorage", onStorageEvent);
+    }
+    $('.play').click(function() {
+        PageSoundPlay();
+    });
+    $('.stop').click(function() {
+        PageSoundStop();
+    });
+});
