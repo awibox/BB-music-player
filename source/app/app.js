e699bd7a63101e613ce334c098355679124a7676
Andrey Arkhipov <wibox@ya.ru>
2016-09-06 01:33:31 +0300

    Наработки по пролистыванию песни

--- source/app/app.js
+++ source/app/app.js
@@ -12,63 +12,12 @@ new Player(data, '#header-wrap');
 new Tracks(data, '#content-wrap');
 
 import './../build/sass/retina.scss';
-import 'jquery-ui-bundle';
-
-var song;
-// var tracker = $('.tracker');
-
-
-function initAudio(elem) {
-    var url = elem.attr('data-src');
-    var title = elem.attr('title');
-
-    var cover = elem.attr('data-cover');
-    // var artist = elem.attr('artist');
-    $('.player__title').text(title);
-    // $('.player .artist').text(artist);
-    $('.player__cover').css('background-image','url('+ cover +')');
-
-    song = new Audio(url);
-
-    // timeupdate event listener
-    song.addEventListener('timeupdate',function (){
-        var curtime = parseInt(song.currentTime, 10);
-        // tracker.slider('value', curtime);
-    });
-
-    $('.tracks li').removeClass('active');
-    elem.addClass('active');
-}
-
-function playAudio() {
-    var nowTime = new Date().getTime();
-    localStorage['song'] = nowTime;
-    song.addEventListener('ended', function () {
-        var next = $('.tracks li.active').next();
-        if (next.length == 0) {
-            next = $('.tracks li:first-child');
-        }
-        initAudio(next);
-        playAudio();
-    }, false);
-    song.play();
+import slider from 'jquery-ui/ui/widgets/slider';
+import 'jquery-ui/themes/base/core.css';
+import 'jquery-ui/themes/base/slider.css';
+import 'jquery-ui/themes/base/theme.css';
 
 
-    // tracker.slider("option", "max", song.duration);
-    $('.fa-play').addClass('hidden');
-    $('.fa-pause').removeClass('hidden');
-}
-function stopAudio() {
-    song.pause();
-    song.currentTime = 0;
-
-    $('.fa-play').removeClass('hidden');
-    $('.fa-pause').addClass('hidden');
-}
-function onStorageEvent(storageEvent){
-    if(storageEvent.key == 'song')
-        stopAudio();
-}
 
 $(document).ready(function(){
     if (window.addEventListener) {
@@ -77,12 +26,14 @@ $(document).ready(function(){
         window.attachEvent("onstorage", onStorageEvent);
     }
 
+    let song,
+        tracker = $('.tracker'),
+        volume = $('.volume');
 
-    // initialization - first element in playlist
     initAudio($('.tracks li:first-child'));
 
     song.volume = 0.8;
-    var volume = $('.volume');
+
     volume.slider({
         range: 'min',
         min: 1,
@@ -94,6 +45,70 @@ $(document).ready(function(){
         },
         stop: function(event,ui) {},
     });
+    tracker.slider({
+        range: 'min',
+        min: 0, max: 10,
+        start: function(event,ui) {},
+        slide: function(event, ui) {
+            song.currentTime = ui.value;
+            console.log(song.currentTime);
+        },
+        stop: function(event,ui) {}
+    });
+
+    function initAudio(elem) {
+        let url = elem.attr('data-src');
+        let title = elem.attr('title');
+
+        let cover = elem.attr('data-cover');
+        // let artist = elem.attr('artist');
+        $('.player__title').text(title);
+        // $('.player .artist').text(artist);
+        $('.player__cover').css('background-image','url('+ cover +')');
+
+        song = new Audio(url);
+
+        // timeupdate event listener
+        song.addEventListener('timeupdate',function (){
+            let curtime = parseInt(song.currentTime, 10);
+            console.log(curtime);
+            tracker.slider('value', curtime);
+            console.log(tracker);
+        });
+        song.addEventListener('ended', function () {
+            let next = $('.tracks li.active').next();
+            if (next.length == 0) {
+                next = $('.tracks li:first-child');
+            }
+            initAudio(next);
+            playAudio();
+        }, false);
+
+        $('.tracks li').removeClass('active');
+        elem.addClass('active');
+    }
+
+    function playAudio() {
+        song.pause();
+        song.play();
+        tracker.slider("option", "max", song.duration);
+
+        let nowTime = new Date().getTime();
+        localStorage['song'] = nowTime;
+
+        $('.fa-play').addClass('hidden');
+        $('.fa-pause').removeClass('hidden');
+    }
+    function stopAudio() {
+        song.pause();
+
+        $('.fa-play').removeClass('hidden');
+        $('.fa-pause').addClass('hidden');
+    }
+    function onStorageEvent(storageEvent){
+        if(storageEvent.key == 'song')
+            stopAudio();
+    }
 
     // play click
     $('.fa-play').click(function (e) {
@@ -113,7 +128,7 @@ $(document).ready(function(){
 
         stopAudio();
 
-        var next = $('.tracks li.active').next();
+        let next = $('.tracks li.active').next();
         if (next.length == 0) {
             next = $('.tracks li:first-child');
         }
@@ -127,7 +142,7 @@ $(document).ready(function(){
 
         stopAudio();
 
-        var prev = $('.tracks li.active').prev();
+        let prev = $('.tracks li.active').prev();
         if (prev.length == 0) {
             prev = $('.tracks li:last-child');
         }
