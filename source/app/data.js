822b2b2e5e533f03569844d8177eb8b58a4735fc
Andrey Arkhipov <wibox@ya.ru>
2016-09-07 14:53:22 +0300

    Новые песни

--- source/app/data.js
+++ source/app/data.js
@@ -44,6 +44,69 @@ var data = {
       time: "02:28",
       src: "mp3/Matti_Paalanen_-_Emotion.mp3",
       cover: "https://imgjam1.jamendo.com/albums/s145/145266/covers/1.50.jpg"
+    },
+    {
+      artist: "Antarctic Breeze",
+      title: "Prometheus",
+      time: "03:55",
+      src: "mp3/ANtarcticbreeze_-_Prometheus.mp3",
+      cover: "https://imgjam2.jamendo.com/track/s1229/1229086/covers/1.50.jpg"
+    },
+    {
+      artist: "Lyonn",
+      title: "The Symphony",
+      time: "03:08",
+      src: "mp3/Lyonn_-_The_Symphony.mp3",
+      cover: "https://imgjam2.jamendo.com/track/s1342/1342712/covers/1.50.jpg"
+    },
+    {
+      artist: "Soundshrim",
+      title: "Ambient Background",
+      time: "02:24",
+      src: "mp3/soundshrim_-_Ambient_Background.mp3",
+      cover: "https://imgjam1.jamendo.com/track/s1320/1320383/covers/1.50.jpg"
+    },
+    {
+      artist: "Skabrot",
+      title: "No Ska Today",
+      time: "04:15",
+      src: "mp3/Skabrot_-_No_Ska_Today.mp3",
+      cover: "https://imgjam1.jamendo.com/albums/s136/136436/covers/1.50.jpg"
+    },
+    {
+      artist: "LukHash",
+      title: "The Other Side",
+      time: "03:33",
+      src: "mp3/LukHash_-_THE_OTHER_SIDE.mp3",
+      cover: "https://imgjam1.jamendo.com/albums/s149/149468/covers/1.50.jpg"
+    },
+    {
+      artist: "Cloz",
+      title: "Placer (Original Mix)",
+      time: "04:27",
+      src: "mp3/Cloz_-_Cloz_-_Placer__Original_Mix_.mp3",
+      cover: "https://imgjam1.jamendo.com/albums/s97/97809/covers/1.50.jpg"
+    },
+    {
+      artist: "Hasenchat",
+      title: "I Never Make It (EDM Mix)",
+      time: "06:17",
+      src: "mp3/Hasenchat_-_04_I_Never_make_it___EDM_Mix__.mp3",
+      cover: "https://imgjam2.jamendo.com/albums/s142/142269/covers/1.50.jpg"
+    },
+    {
+      artist: "Gigatop",
+      title: "Turn all the lights",
+      time: "03:20",
+      src: "mp3/Gigatop_-_Turn_all_the_lights.mp3",
+      cover: "https://imgjam2.jamendo.com/albums/s148/148857/covers/1.50.jpg"
+    },
+    {
+      artist: "Gigatop",
+      title: "Storm",
+      time: "03:38",
+      src: "mp3/Gigatop_-_Storm.mp3",
+      cover: "https://imgjam2.jamendo.com/albums/s148/148857/covers/1.50.jpg"
     }
   ]
 };
